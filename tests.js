1e487d441fc32f0ff86554857bf62a443a5c5800
Pavel Nedzelskiy <pavel.nedzelskiy@dukascopy.com>
2017-11-13 15:48:09 +0200

    continue writting tests

--- tests.js
+++ tests.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const availableCommands = {};
-const { expect, assert } = require('chai');
+const sinon = require('sinon');
 const EventEmitter = require('events');
+const { expect, assert  } = require('chai');
+
 const execConsole = require('./index.js')(availableCommands);
 
 let stdoutMock = {
@@ -18,13 +20,26 @@ let stdoutMock = {
 
 let stdinMock = new EventEmitter();
 
-stdinMock.setRawMode = function(bool) {
-    return bool;
+stdinMock.setRawMode = function() {
+    stdinMock.setRawMode.countCalls++;
+    stdinMock.setRawMode.args.push(arguments);
 };
-stdinMock.resume = function() {};
-stdinMock.setEncoding = function(str) {
-    return str;
+stdinMock.setRawMode.countCalls = 0;
+stdinMock.setRawMode.args = [];
+
+stdinMock.resume = function() {
+    stdinMock.resume.countCalls++;
+    stdinMock.resume.args.push(arguments);
 };
+stdinMock.resume.countCalls = 0;
+stdinMock.resume.args = [];
+
+stdinMock.setEncoding = function() {
+    stdinMock.setEncoding.countCalls++;
+    stdinMock.setEncoding.args.push(arguments);
+};
+stdinMock.setEncoding.countCalls = 0;
+stdinMock.setEncoding.args = [];
 
 execConsole.controls.stdout = stdoutMock;
 execConsole.controls.stdin = stdinMock;
@@ -53,7 +68,6 @@ describe('JS console command executor', () => {
     });
     it('should deny write to stdout special symbols expect declared in object keys', next => {
         let testKey = execConsole.actions.testKeyForAvailableToStdout;
-        let toUnicode = execConsole.actions.toUnicode;
         for (let i = 0; i < 32; i++) {
             for (let key in execConsole.keys) {
                 if (!execConsole.keys.hasOwnProperty(key)) continue;
@@ -99,9 +113,28 @@ describe('JS console command executor', () => {
         next();
     });
     it('should shows previous command after pressed up arrow', next => {
-        stdinMock.emit('data', 'd');
-        console.log(stdoutMock.buffer);
+        let spy = sinon.spy(execConsole.actions, "goUpToCommandsHistory");
+        stdinMock.emit('data', '\u001B\u005B\u0041'); // up arrow
+        stdinMock.emit('data', 's');
+        assert.isTrue(spy.calledOnce);
+        expect(stdoutMock.buffer).to.be.equal('s');
+        // expect(spy).to.have.been.called();
         // goUpToCommandsHistory
+        next();
+    });
+
+    it('should runs in raw mode', next => {
+        expect(stdinMock.setRawMode.countCalls).to.be.equal(1);
+        assert.isTrue(stdinMock.setRawMode.args[0][0]);
+        next();
+    });
+    it('should runs resume', next => {
+        expect(stdinMock.resume.countCalls).to.be.equal(1);
+        next();
+    });
+    it('should runs in "utf-8" encoding', next => {
+        expect(stdinMock.setEncoding.countCalls).to.be.equal(1);
+        expect(stdinMock.setEncoding.args[0][0].toLocaleLowerCase().replace('-', '')).to.be.equal('utf8');
         next();
     });
 });
